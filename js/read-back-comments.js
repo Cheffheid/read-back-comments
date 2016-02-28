28379b383988fb7664cedf6dde6ccd4d71912d1b
Jeffrey de Wit <Nasgath@gmail.com>
2016-02-28 12:18:23 -0500

    First pass at this read back thing

--- js/read-back-comments.js
+++ js/read-back-comments.js
@@ -1,1 +1,33 @@
-console.log("Yatta!");
+(function readBackComments(){
+    "use strict";
+
+    if ( 'speechSynthesis' in window ) {
+        var commentForm = document.forms['commentform'];
+
+        if ( commentForm ) {
+            var readBackParagraph = document.createElement('p');
+            var readBackButton = document.createElement('button');
+            var submitWrap = commentForm.getElementsByClassName('form-submit');
+
+            readBackParagraph.className += 'read-back-button';
+
+            readBackButton.innerHTML = "Read back comment";
+            readBackButton.onclick = function(e) {
+                e.preventDefault();
+
+                var commentText = commentForm.querySelector('textarea#comment');
+                var commentMessage = new SpeechSynthesisUtterance( 'You did not type in a comment.' );
+
+                if ( commentText && commentText.value !== '' ) {
+                    var commentMessage = new SpeechSynthesisUtterance( commentText.value );
+                }
+
+                window.speechSynthesis.speak( commentMessage );
+            }
+
+            readBackParagraph.appendChild( readBackButton );
+
+            commentForm.insertBefore( readBackParagraph, submitWrap[0] );
+        }
+    }
+})();
