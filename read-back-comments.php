cf279f07662e120bb2e1f29b25ba48602caba9f8
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-08-30 10:24:06 -0400

    Minor code clean up

--- read-back-comments.php
+++ read-back-comments.php
@@ -18,10 +18,8 @@
 * Text Domain:       read-back-comments
 * Domain Path:       /languages
 *
-*
-*
 * This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License, version 2, as 
+* it under the terms of the GNU General Public License, version 2, as
 * published by the Free Software Foundation.
 *
 * This program is distributed in the hope that it will be useful,
@@ -36,32 +34,34 @@
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
 class Read_Back_Comments {
 
-    private $plugin_name;
-    private $plugin_version;
+	private $plugin_name;
+	private $plugin_version;
 
-    public function __construct() {
-        $this->plugin_name = 'read-back-comments';
-        $this->plugin_version = '1.0';
+	public function __construct() {
+		$this->plugin_name = 'read-back-comments';
+		$this->plugin_version = '1.0';
 
-        add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_read_back_script' ) );
-    }
+		add_action( 'wp_enqueue_scripts', array( &$this, 'enqueue_read_back_script' ) );
+	}
 
-    public function enqueue_read_back_script() {
-        if ( comments_open() ) {
-            wp_enqueue_script( $this->plugin_name, 
-                                plugin_dir_url( __FILE__ ) . 'js/read-back-comments.js',
-                                array(),
-                                $this->plugin_version,
-                                true
-                             );
-        }
-    }
+	public function enqueue_read_back_script() {
+		if ( ! comments_open() ) {
+			return;
+		}
 
+		wp_enqueue_script(
+			$this->plugin_name,
+			plugin_dir_url( __FILE__ ) . 'js/read-back-comments.js',
+			array(),
+			$this->plugin_version,
+			true
+		);
+	}
 }
 
 $read_back_comments = new Read_Back_Comments();
